d696917940a251d26153d46756a8b7d553ce0f95
2019-Q2-SOW-DEV-029 <plzworknow11@gmail.com>
2019-06-11 19:48:49 +0200

    feature: Calculate score for frames without bonus

--- __tests__/bowlingGame.spec.js
+++ __tests__/bowlingGame.spec.js
@@ -14,4 +14,15 @@ describe("Bowling game", () => {
         expect(bowlingGame.calculateScore()).toEqual(0);
     });
 
+    it("should calculate the score for frames without bonus", () => {
+        let roll = 0;
+        for (; roll < 10; roll++) {
+            bowlingGame.roll(1);
+        }
+        for (; roll < 20; roll++) {
+            bowlingGame.roll(2);
+        }
+
+        expect(bowlingGame.calculateScore()).toEqual(30);
+    });
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,7 +1,18 @@
 module.exports = bowlingGame;
 
-function bowlingGame(){
-    this.calculateScore = function(){
-        return 0;
+function bowlingGame() {
+    this.scoreBoard = [];
+    let bowlingGame = this;
+
+    this.roll = function (pinsScored) {
+        bowlingGame.scoreBoard.push(pinsScored);
+    }
+
+    this.calculateScore = function () {
+        let score = 0;
+        for (let roll = 0; roll < bowlingGame.scoreBoard.length; roll++) {
+            score += bowlingGame.scoreBoard[roll];
+        }
+        return score;
     }
 }
