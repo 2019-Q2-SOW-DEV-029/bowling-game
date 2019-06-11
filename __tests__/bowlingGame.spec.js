78b2ea9b9ba7c9d133622b72a79a2bf15a054e10
2019-Q2-SOW-DEV-029 <plzworknow11@gmail.com>
2019-06-11 19:39:35 +0200

    test: Extract common code to beforeEach

--- __tests__/bowlingGame.spec.js
+++ __tests__/bowlingGame.spec.js
@@ -2,15 +2,15 @@ let BowlingGame = require("../src/bowlingGame");
 let bowlingGame;
 
 describe("Bowling game", () => {
-    it("should have a function to calculate score ", () => {
+    beforeEach(() => {
         bowlingGame = new BowlingGame();
+    });
 
+    it("should have a function to calculate score ", () => {
         expect(bowlingGame.calculateScore).toBeDefined();
     });
 
     it("should calculate the score for gutter game to 0", () => {
-        bowlingGame = new BowlingGame();
-        
         expect(bowlingGame.calculateScore()).toEqual(0);
     });
 
